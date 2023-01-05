14ffcb784adb9d4e885095b91ab4da0b7e65bbd3
Mateusz Weclawowicz <S00224403@atu.ie>
2023-01-05 10:03:43 +0000

    Q1(e)

---
+++ Exam/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Team
+    {
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
