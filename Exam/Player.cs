b381e36ade4a6c07f41f66651b8ce79a472c855a
Mateusz Weclawowicz <S00224403@atu.ie>
2023-01-05 10:36:21 +0000

    Q1(i)

--- Exam/Player.cs
+++ Exam/Player.cs
@@ -8,15 +8,41 @@ namespace Exam
 {
     internal class Player
     {
+        private const int WIN = 3, DRAW = 1, LOSS = 0;
+        private int _points;
         #region Properties
 
         public string Name { get; set; }
         public string ResultRecord { get; set; }
+        public int Points 
+        { 
+            get
+            {
+                _points = 0;
+                for (int i = 0; i < ResultRecord.Length; i++)
+                {
+                    if (ResultRecord[i] == 'W')
+                    {
+                        _points += WIN;
+                    }
+                    else if(ResultRecord[i] == 'D')
+                    {
+                        _points += DRAW;
+                    }
+                    else
+                    {
+                        _points += LOSS;
+                    }
+                }
+                return _points;
+            }
+        }
 
         #endregion Properties
+
         public override string ToString()
         {
-            return String.Format($"{Name} - {ResultRecord}");
+            return String.Format($"{Name} - {ResultRecord} - {Points}");
         }
     }
 }
